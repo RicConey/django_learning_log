a01a0a335940511bb2daf8e79359fcf885393193
RicConey <82566623+RicConey@users.noreply.github.com>
2021-06-01 20:51:34 +0300

    Update settings.py

--- leaning_log/settings.py
+++ leaning_log/settings.py
@@ -139,3 +139,8 @@ LOGIN_URL = '/users/login/'
 # #Настройки Heroku
 # import django_heroku
 # django_heroku.settings(locals())
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
